024aca1b619de42bc3782a36a1b66ec11a90608e
ahomiya.com <me@ahomiya.com>
2014-08-07 11:07:16 +0700

    Include jQuery browser and OS detection plugin

--- htdocs/js/plugins.js
+++ htdocs/js/plugins.js
@@ -1,6 +1,170 @@
 // Plugins
 // jQuery plugins and other 3rd party scripts
 // -----------------------------------------------------------------------------
+// jQuery browser and OS detection plugin
+
+// http://www.stoimen.com/blog/2009/07/16/jquery-browser-and-os-detection-plugin/
+// http://www.stoimen.com/blog/2009/07/04/jquery-os-detection/
+// http://www.quirksmode.org/js/detect.html
+
+(function() {
+
+  var clientDetect = {
+    init: function () {
+      this.browser  = this.searchString(this.dataBrowser) || "An unknown browser";
+      this.version  = this.searchVersion(navigator.userAgent) || this.searchVersion(navigator.appVersion) || "An unknown version";
+      this.OS       = this.searchString(this.dataOS) || "An unknown OS";
+    },
+    searchString: function (data) {
+      for (var i=0; i<data.length; i++) {
+        var dataString = data[i].string;
+        var dataProp   = data[i].prop;
+        this.versionSearchString = data[i].versionSearch || data[i].identity;
+        if (dataString) {
+          if (dataString.indexOf(data[i].subString) != -1) {
+            return data[i].identity;
+          }
+        }
+        else if (dataProp) {
+          return data[i].identity;
+        }
+      }
+    },
+    searchVersion: function (dataString) {
+      var index = dataString.indexOf(this.versionSearchString);
+      if (index == -1) return;
+      return parseFloat(dataString.substring(index+this.versionSearchString.length+1));
+    },
+
+    /* Browser */
+    dataBrowser: [
+      {
+        string        : navigator.userAgent,
+        subString     : "Chrome",
+        identity      : "Browser-Chrome"
+      },
+      {
+        string        : navigator.userAgent,
+        subString     : "OmniWeb",
+        versionSearch : "OmniWeb/",
+        identity      : "Browser-OmniWeb"
+      },
+      {
+        string        : navigator.vendor,
+        subString     : "Apple",
+        identity      : "Browser-Safari",
+        versionSearch : "Version"
+      },
+      {
+        prop          : window.opera,
+        identity      : "Browser-Opera"
+      },
+      {
+        string        : navigator.vendor,
+        subString     : "iCab",
+        identity      : "Browser-iCab"
+      },
+      {
+        string        : navigator.vendor,
+        subString     : "KDE",
+        identity      : "Browser-Konqueror"
+      },
+      {
+        string        : navigator.userAgent,
+        subString     : "Firefox",
+        identity      : "Browser-Firefox"
+      },
+      {
+        string        : navigator.vendor,
+        subString     : "Camino",
+        identity      : "Browser-Camino"
+      },
+      {
+        // For newer Netscapes (6+)
+        string        : navigator.userAgent,
+        subString     : "Netscape",
+        identity      : "Browser-Netscape"
+      },
+      {
+        string        : navigator.userAgent,
+        subString     : "MSIE",
+        identity      : "Browser-IE",
+        versionSearch : "MSIE"
+      },
+      {
+        string        : navigator.userAgent,
+        subString     : "Gecko",
+        identity      : "Browser-Mozilla",
+        versionSearch : "rv"
+      },
+      {
+        // For older Netscapes (4-)
+        string        : navigator.userAgent,
+        subString     : "Mozilla",
+        identity      : "Netscape",
+        versionSearch : "Browser-Mozilla"
+      }
+    ],
+
+    /* OS */
+    dataOS: [
+      {
+        string        : navigator.platform,
+        subString     : "Win",
+        identity      : "OS-Windows"
+      },
+      {
+        string        : navigator.platform,
+        subString     : "Mac",
+        identity      : "OS-Mac"
+      },
+      {
+        string        : navigator.userAgent,
+        subString     : "iOS",
+        identity      : "OS-iOS"
+      },
+      {
+        string        : navigator.platform,
+        subString     : "Linux",
+        identity      : "OS-Linux"
+      }
+    ]
+
+  };
+
+  // Initialize
+  clientDetect.init();
+  window.$.client = {
+    os      : clientDetect.OS,
+    browser : clientDetect.browser
+  };
+
+  // Legacy IE
+  // http://n33.co/2013/03/23/browser-on-jquery-19x-for-legacy-ie-detection
+  var msie = document.documentMode;
+  $.client.msie = !!msie;
+  $.client.version = msie || 0;
+
+})();
+
+
+// Add CSS classes to the HTML element
+$('html')
+  .addClass($.client.os)          // OS
+  .addClass($.client.browser);    // Browser
+
+if ($.client.msie) {              // Internet Explorer
+  $('html')
+    .addClass('MSIE-' + $.client.version);
+
+  // Hack - IE11
+  if ($.client.version == 11) {
+    $('html')
+      .removeClass('Browser-Mozilla')
+      .addClass('Browser-IE');
+  }
+}
+// -----------------------------------------------------------------------------
 // HTML5 Boilerplate
 // https://github.com/h5bp/html5-boilerplate
 
